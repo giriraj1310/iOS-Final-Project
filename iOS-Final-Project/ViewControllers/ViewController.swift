79384e8969e82d38c5548e0bd910ae69ce8210c7
Laurence <chongl@sheridancollega.ca>
2019-04-04 09:21:49 -0400

    Test

--- iOS-Final-Project/ViewControllers/ViewController.swift
+++ iOS-Final-Project/ViewControllers/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    Test Commit and some comments:
+//    This view controller should check if the user has already logged in once (userdefault)
+//    if the user has logged in already, segue into the default feed page,
+//    if not, then segue into login page.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
